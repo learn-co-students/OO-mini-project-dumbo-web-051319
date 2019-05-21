dd68496fc082c2a1f310a2ec37e579ec5be6b42d
April <fanmeng0417@gmail.com>
2019-05-20 21:47:11 -0400

    Done.

--- app/models/User.rb
+++ app/models/User.rb
@@ -36,4 +36,35 @@ class User
     # binding.pry
     self.recipes.select{|recipecard| recipecard.date[0..1] == self.recipes.sort_by{|recipecard| recipecard.date[0..1].to_i}[-1].date[0..1]}.sort_by{|recipecard| recipecard.date[3..4].to_s}[-1].recipe
   end
+  def safe_recipes
+    #get allergy of the user
+    allergytemp = Allergy.all.select {|allergy| allergy.user == self}
+    #get allergy ingredient
+    allergy_ingredient_temp = allergytemp.map {|allergy| allergy.ingredient}
+    #get recipe of the user
+    recipetemp = RecipeCard.all.select {|recipecard| recipecard.user == self}.map {|recipecard| recipecard.recipe}
+    #get ingredients of all recipe
+    ## creating a recipe_with_ingredients Hash
+    recipe_with_ingredients = {}
+    recipetemp.each {|recipe| recipe_with_ingredients[recipe]}
+    recipetemp.each do |recipe|
+      RecipeIngredient.all.each do |recipeingredient|
+        if recipeingredient.recipe == recipe
+          recipe_with_ingredients[recipe] = recipeingredient.ingredient.flatten
+        end
+      end
+    end
+    #get the safe_recipes
+    recipe_with_ingredients.map do |recipe, ingredients|
+      counter = 0
+      ingredients.each do |ingredient|
+        if allergy_ingredient_temp.include?(ingredient)
+          counter +=1
+        end
+      end
+      if counter == 0
+        recipe
+      end
+    end.compact
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,13 +7,14 @@ ken_u = User.new("Ken")
 pencake_r = Recipe.new("Pencake")
 chocolate_r = Recipe.new("Chocolate")
 fruitpunch_r = Recipe.new("Fruitpunch")
+burger_r= Recipe.new("Burger")
 
 pencake_rc_tom_u = tom_u.add_recipe_card("Pencake_RC_Tom", pencake_r, "05/10", 7)
 pencake_rc_ken_u = ken_u.add_recipe_card("Pencake_RC_Tom", pencake_r, "04/12", 10)
 chocolate_rc_tim_u = tim_u.add_recipe_card("Chocolate_RC_Tim", chocolate_r,"03/20", 2)
 fruitpunch_rc_tim_u = tim_u.add_recipe_card("Fruitpunch_RC_Tim", fruitpunch_r, "04/20", 4)
 pencake_rc_tim_u = tim_u.add_recipe_card("Pencake_RC_Tim", pencake_r, "04/10", 10)
-
+burger_rc_tim_u = tim_u.add_recipe_card("Burger_RC_Tim", burger_r, "04/21", 6)
 
 egg_i = Ingredient.new("egg")
 coco_i = Ingredient.new("coco")
@@ -21,6 +22,9 @@ butter_i = Ingredient.new("butter")
 milk_i = Ingredient.new("milk")
 onion_i = Ingredient.new("onion")
 galic_i = Ingredient.new("galic")
+water_i = Ingredient.new("water")
+juice_i = Ingredient.new("juice")
+beef_i = Ingredient.new("beef")
 
 egg_all = ken_u.declare_allergy("Egg Allergy", egg_i)
 butter_all = tim_u.declare_allergy("Butter Allergy", butter_i)
@@ -29,6 +33,8 @@ milk_all = tim_u.declare_allergy("Milk Allergy", milk_i)
 
 pencake_ri = pencake_r.add_ingredients("Pencake_RI",egg_i, butter_i, milk_i)
 chocolate_ri = chocolate_r.add_ingredients("Chocolate_RI", coco_i, milk_i)
+fruitpunch_ri = fruitpunch_r.add_ingredients("Fruitpunch_RI", water_i, juice_i)
+burger_ri = burger_r.add_ingredients("Burger_RI", egg_i, galic_i, onion_i, beef_i)
 
 
 binding.pry
